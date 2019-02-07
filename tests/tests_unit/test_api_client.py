93cdd70074b813a1b358efa80ef30cac6a86067f
erlendcognite <erlend.vollset@cognite.com>
2019-02-07 22:34:25 +0100

    Implement async api client and start cdp client
    Handle api_version in api_client
    
    Make requested changes

---
+++ tests/tests_unit/test_api_client.py
@@ -0,0 +1,142 @@
+import os
+
+import pytest
+from aioresponses import aioresponses
+
+from cognite_data_fetcher._client.api_client import (
+    DEFAULT_BASE_URL,
+    DEFAULT_NUM_OF_RETRIES,
+    ApiClient,
+    ApiKeyError,
+    DataFetcherHttpError,
+)
+from tests.utils import BASE_URL, BASE_URL_V0_5, run_until_complete
+
+
+@pytest.fixture
+def unset_env_api_key():
+    tmp_key = os.environ.pop("COGNITE_API_KEY")
+    yield
+    os.environ["COGNITE_API_KEY"] = tmp_key
+
+
+def test_create_client_no_api_key(unset_env_api_key):
+    with pytest.raises(ApiKeyError) as e:
+        ApiClient()
+
+    assert "No API key was specified" == e.value.args[0]
+
+
+def test_create_client_invalid_api_key():
+    with aioresponses() as mocked:
+        mocked.get(BASE_URL + "/login/status", payload={"data": {"loggedIn": False}})
+
+        with pytest.raises(ApiKeyError) as e:
+            ApiClient()
+
+    assert "Invalid API Key" == e.value.args[0]
+
+
+def test_create_default_client(mock_async_response):
+    client = ApiClient()
+
+    assert DEFAULT_BASE_URL == client._base_url
+    assert DEFAULT_NUM_OF_RETRIES == client._num_of_retries
+    assert "test" == client._project
+
+
+@pytest.fixture
+def set_environment_config():
+    os.environ["COGNITE_NUM_RETRIES"] = "0"
+    os.environ["COGNITE_BASE_URL"] = "test"
+    os.environ["COGNITE_PROJECT"] = "test"
+    yield
+    del os.environ["COGNITE_NUM_RETRIES"]
+    del os.environ["COGNITE_BASE_URL"]
+    del os.environ["COGNITE_PROJECT"]
+
+
+def test_create_client_environment_config(mock_async_response, set_environment_config):
+    client = ApiClient()
+
+    assert "test" == client._base_url
+    assert "test" == client._project
+    assert 0 == client._num_of_retries
+
+
+def test_create_client_param_config(mock_async_response):
+    client = ApiClient(api_key="test", project="test", base_url="test", num_of_retries=0)
+
+    assert "test" == client._api_key
+    assert "test" == client._base_url
+    assert "test" == client._project
+    assert 0 == client._num_of_retries
+
+
+@pytest.fixture
+def mock_response_all_http_methods_200(mock_async_response):
+    mock_response_body = {"data": "something"}
+    mock_async_response.get(BASE_URL_V0_5 + "/any", status=200, payload=mock_response_body)
+    mock_async_response.post(BASE_URL_V0_5 + "/any", status=200, payload=mock_response_body)
+    mock_async_response.delete(BASE_URL_V0_5 + "/any", status=200, payload=mock_response_body)
+    yield mock_response_body
+
+
+def test_get(mock_response_all_http_methods_200):
+    client = ApiClient()
+
+    res = run_until_complete(client.get(url="/any"))
+    assert mock_response_all_http_methods_200 == res
+
+
+def test_post(mock_response_all_http_methods_200):
+    client = ApiClient()
+
+    res = run_until_complete(client.post(url="/any", body={"blabla": "blabla"}))
+    assert mock_response_all_http_methods_200 == res
+
+
+def test_delete(mock_response_all_http_methods_200):
+    client = ApiClient()
+
+    res = run_until_complete(client.delete(url="/any"))
+    assert mock_response_all_http_methods_200 == res
+
+
+@pytest.fixture
+def mock_response_all_http_methods_400(mock_async_response):
+    mock_response_body = {"error": {"message": "bla"}}
+    mock_async_response.get(BASE_URL_V0_5 + "/any", status=400, payload=mock_response_body)
+    mock_async_response.post(BASE_URL_V0_5 + "/any", status=400, payload=mock_response_body)
+    mock_async_response.delete(BASE_URL_V0_5 + "/any", status=400, payload=mock_response_body)
+    yield mock_response_body
+
+
+def test_get_error(mock_response_all_http_methods_400):
+    client = ApiClient(num_of_retries=0)
+
+    with pytest.raises(DataFetcherHttpError) as e:
+        run_until_complete(client.get(url="/any"))
+
+    assert "bla" == e.value.message
+    assert 400 == e.value.code
+
+
+def test_post_error(mock_response_all_http_methods_400):
+    client = ApiClient()
+
+    with pytest.raises(DataFetcherHttpError) as e:
+        run_until_complete(client.post(url="/any", body={"blabla": "blabla"}))
+
+    assert "bla" == e.value.message
+    assert 400 == e.value.code
+
+
+def test_delete_error(mock_response_all_http_methods_400):
+    client = ApiClient(num_of_retries=0)
+
+    with pytest.raises(DataFetcherHttpError) as e:
+        run_until_complete(client.delete(url="/any"))
+
+    assert "bla" == e.value.message
+    assert 400 == e.value.code
---
+++ tests/utils.py
@@ -0,0 +1,12 @@
+import asyncio
+
+BASE_URL = "https://api.cognitedata.com"
+BASE_URL_V0_5 = BASE_URL + "/api/0.5/projects/test"
+BASE_URL_V0_6 = BASE_URL + "/api/0.6/projects/test"
+
+
+def run_until_complete(*futures):
+    res = asyncio.get_event_loop().run_until_complete(asyncio.gather(*futures))
+    if len(futures) == 1:
+        return res[0]
+    return res
