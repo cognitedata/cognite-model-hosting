f70f5b74496321fd38cf2eec6f7f81bd7501c1c1
Nils Barlaug <nils.barlaug@cognite.com>
2019-02-11 14:08:49 +0100

    First draft of DataSpec

--- cognite/data_fetcher/data_spec.py
+++ cognite/data_fetcher/data_spec.py
@@ -1,3 +1,176 @@
-class DataSpec:
-    def __init__(self):
-        pass
+import json
+from typing import Dict
+
+from marshmallow import RAISE, Schema, ValidationError, fields, post_dump, post_load, validates_schema
+
+
+class _BaseSpec:
+    _schema = None  # Set by subclass
+
+    def dump(self):
+        dumped = self._schema.dump(self)
+        errors = self._schema.validate(dumped)
+        if errors:
+            raise ValidationException(errors)
+
+        return dumped
+
+    def validate(self):
+        self.dump()
+
+    @classmethod
+    def load(cls, data):
+        try:
+            return cls._schema.load(data)
+        except ValidationError as e:
+            raise ValidationException(e.messages) from e
+
+    def to_json(self):
+        return json.dumps(self.dump(), indent=4, sort_keys=True)
+
+    @classmethod
+    def from_json(cls, s: str):
+        return cls.load(json.loads(s))
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+
+class TimeSeriesSpec(_BaseSpec):
+    def __init__(
+        self,
+        id: int,
+        start: int,
+        end: int,
+        aggregate: str = None,
+        granularity: str = None,
+        include_outside_points: bool = None,
+    ):
+        self.id = id
+        self.start = start
+        self.end = end
+        self.aggregate = aggregate
+        self.granularity = granularity
+        self.include_outside_points = include_outside_points
+
+
+class ScheduleTimeSeriesSpec(_BaseSpec):
+    def __init__(self, id: int, aggregate: str = None, granularity: str = None, include_outside_points: bool = None):
+        self.id = id
+        self.aggregate = aggregate
+        self.granularity = granularity
+        self.include_outside_points = include_outside_points
+
+
+class FileSpec(_BaseSpec):
+    def __init__(self, id: int):
+        self.id = id
+
+
+class DataSpec(_BaseSpec):
+    def __init__(self, time_series: Dict[str, TimeSeriesSpec] = None, files: Dict[str, FileSpec] = None):
+        self.time_series = time_series or {}
+        self.files = files or {}
+
+
+class ScheduleDataSpec(_BaseSpec):
+    def __init__(self, stride: str, window_size: str, time_series: Dict[str, ScheduleTimeSeriesSpec] = None):
+        self.stride = stride
+        self.window_size = window_size
+        self.time_series = time_series or {}
+
+
+class ValidationException(Exception):
+    def __init__(self, errors):
+        self.errors = errors
+
+    def __str__(self):
+        return json.dumps(self.errors, indent=4, sort_keys=True)
+
+
+class _BaseSchema(Schema):
+    _ignore_values = [None, {}]
+    _default_spec = None
+
+    def __init__(self, *args, **kwargs):
+        self._spec = kwargs.get("spec", None) or self._default_spec
+        assert self._spec is not None
+        if "spec" in kwargs:
+            kwargs.pop("spec")
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        unknown = RAISE
+
+    @post_dump
+    def remove_none(self, data):
+        return {k: v for k, v in data.items() if v not in self._ignore_values}
+
+    @post_load
+    def to_spec(self, data):
+        return self._spec(**data)
+
+
+class _TimeSeriesSpecSchema(_BaseSchema):
+    _default_spec = TimeSeriesSpec
+
+    id = fields.Int(required=True)
+    start = fields.Int(required=True)
+    end = fields.Int(required=True)
+    aggregate = fields.Str()
+    granularity = fields.Str()
+    includeOutsidePoints = fields.Bool(attribute="include_outside_points")
+
+    @validates_schema(skip_on_field_errors=False)
+    def validate_aggregate(self, data):
+        errors = {}
+
+        if "aggregate" in data:
+            if "granularity" not in data:
+                errors["granularity"] = ["granularity must be specified for aggregates."]
+            if "include_outside_points" in data and data["include_outside_points"]:
+                errors["includeOutsidePoints"] = ["Can't include outside points for aggregates."]
+
+        if errors:
+            raise ValidationError(errors)
+
+
+class _FileSpecSchema(_BaseSchema):
+    _default_spec = FileSpec
+
+    id = fields.Int(required=True)
+
+
+class _DataSpecSchema(_BaseSchema):
+    _default_spec = DataSpec
+
+    timeSeries = fields.Dict(keys=fields.Str(), values=fields.Nested(_TimeSeriesSpecSchema), attribute="time_series")
+    files = fields.Dict(keys=fields.Str(), values=fields.Nested(_FileSpecSchema))
+
+
+class _ScheduleDataSpecSchema(_BaseSchema):
+    _default_spec = ScheduleDataSpec
+
+    stride = fields.Str(required=True)
+    windowSize = fields.Str(required=True, attribute="window_size")
+    timeSeries = fields.Dict(
+        keys=fields.Str(),
+        values=fields.Nested(_TimeSeriesSpecSchema(spec=ScheduleTimeSeriesSpec, exclude=("start", "end"))),
+        attribute="time_series",
+    )
+
+
+TimeSeriesSpec._schema = _TimeSeriesSpecSchema()
+ScheduleTimeSeriesSpec._schema = _TimeSeriesSpecSchema(spec=ScheduleTimeSeriesSpec, exclude=("start", "end"))
+FileSpec._schema = _FileSpecSchema()
+DataSpec._schema = _DataSpecSchema()
+ScheduleDataSpec._schema = _ScheduleDataSpecSchema()
--- tests/tests_unit/test_data_spec.py
+++ tests/tests_unit/test_data_spec.py
@@ -1,5 +1,159 @@
-from cognite.data_fetcher.data_spec import DataSpec
+import json
+from collections import namedtuple
+
+import pytest
+
+from cognite.data_fetcher.data_spec import (
+    DataSpec,
+    FileSpec,
+    ScheduleDataSpec,
+    ScheduleTimeSeriesSpec,
+    TimeSeriesSpec,
+    ValidationException,
+)
+
+TestCase = namedtuple("TestCase", ["name", "obj", "primitive"])
+InvalidTestCase = namedtuple("TestCase", ["name", "obj", "primitive", "errors"])
+
+valid_test_cases = [
+    TestCase("minimal_file_spec", FileSpec(id=6), {"id": 6}),
+    TestCase("minimal_time_series_spec", TimeSeriesSpec(id=6, start=123, end=234), {"id": 6, "start": 123, "end": 234}),
+    TestCase(
+        "time_series_include_outside_points",
+        TimeSeriesSpec(id=6, start=123, end=234, include_outside_points=True),
+        {"id": 6, "start": 123, "end": 234, "includeOutsidePoints": True},
+    ),
+    TestCase(
+        "time_series_aggregate",
+        TimeSeriesSpec(id=6, start=123, end=234, aggregate="avg", granularity="1m"),
+        {"id": 6, "start": 123, "end": 234, "aggregate": "avg", "granularity": "1m"},
+    ),
+    TestCase(
+        "schedule_time_series",
+        ScheduleTimeSeriesSpec(id=6, aggregate="avg", granularity="1m"),
+        {"id": 6, "aggregate": "avg", "granularity": "1m"},
+    ),
+    TestCase("empty_data_spec", DataSpec(), {}),
+    TestCase(
+        "full_data_spec",
+        DataSpec(
+            time_series={
+                "ts1": TimeSeriesSpec(id=6, start=123, end=234),
+                "ts2": TimeSeriesSpec(id=7, start=1234, end=2345),
+            },
+            files={"f1": FileSpec(id=3), "f2": FileSpec(id=4)},
+        ),
+        {
+            "timeSeries": {"ts1": {"id": 6, "start": 123, "end": 234}, "ts2": {"id": 7, "start": 1234, "end": 2345}},
+            "files": {"f1": {"id": 3}, "f2": {"id": 4}},
+        },
+    ),
+    TestCase(
+        "minimal_schedule_data_spec",
+        ScheduleDataSpec(stride="1m", window_size="5m"),
+        {"stride": "1m", "windowSize": "5m"},
+    ),
+    TestCase(
+        "full_schedule_data_spec",
+        ScheduleDataSpec(
+            stride="1m",
+            window_size="5m",
+            time_series={"ts1": ScheduleTimeSeriesSpec(id=6), "ts2": ScheduleTimeSeriesSpec(id=7)},
+        ),
+        {"stride": "1m", "windowSize": "5m", "timeSeries": {"ts1": {"id": 6}, "ts2": {"id": 7}}},
+    ),
+]
+
+invalid_test_cases = [
+    InvalidTestCase("file_missing_id", FileSpec(id=None), {}, {"id": ["Missing data for required field."]}),
+    InvalidTestCase(
+        "time_series_missing_fields",
+        TimeSeriesSpec(id=None, start=None, end=None),
+        {},
+        {
+            "id": ["Missing data for required field."],
+            "start": ["Missing data for required field."],
+            "end": ["Missing data for required field."],
+        },
+    ),
+    InvalidTestCase(
+        "time_series_aggregates_but_not_granularity",
+        TimeSeriesSpec(id=6, start=123, end=234, aggregate="avg"),
+        {"id": 6, "start": 123, "end": 234, "aggregate": "avg"},
+        {"granularity": ["granularity must be specified for aggregates."]},
+    ),
+    InvalidTestCase(
+        "time_series_aggregates_but_include_outside_points",
+        TimeSeriesSpec(id=6, start=123, end=234, aggregate="avg", granularity="1m", include_outside_points=True),
+        {"id": 6, "start": 123, "end": 234, "aggregate": "avg", "granularity": "1m", "includeOutsidePoints": True},
+        {"includeOutsidePoints": ["Can't include outside points for aggregates."]},
+    ),
+    InvalidTestCase(
+        "schedule_time_series_with_start_end",
+        ScheduleTimeSeriesSpec,
+        {"id": 6, "start": 123, "end": 234},
+        {"start": ["Unknown field."], "end": ["Unknown field."]},
+    ),
+    InvalidTestCase(
+        "schedule_data_spec_missing_fields",
+        ScheduleDataSpec(window_size=None, stride=None),
+        {},
+        {"windowSize": ["Missing data for required field."], "stride": ["Missing data for required field."]},
+    ),
+]
 
 
-def test_create_data_spec():
-    data_spec = DataSpec()
+@pytest.mark.parametrize("name, obj, primitive", valid_test_cases)
+def test_valid_dump(name, obj, primitive):
+    dumped = obj.dump()
+    assert dumped == primitive, "\nDumped:\n{}\nPrimitive:\n{}\n".format(dumped, primitive)
+
+
+@pytest.mark.parametrize("name, obj, primitive", valid_test_cases)
+def test_valid_load(name, obj, primitive):
+    loaded = obj.__class__.load(primitive)
+    assert loaded == obj, "\nLoaded: ({})\n{}\nObj: ({})\n{}\n".format(type(loaded), loaded, type(obj), obj)
+
+
+@pytest.mark.parametrize("name, obj, primitive", valid_test_cases)
+def test_valid_json_serializable(name, obj, primitive):
+    json_data = obj.to_json()
+    from_json = obj.__class__.from_json(json_data)
+    assert from_json == obj, "\nFrom JSON: ({})\n{}\nObj:({})\n{}\n".format(type(from_json), from_json, type(obj), obj)
+
+
+@pytest.mark.parametrize("name, obj, primitive, errors", invalid_test_cases)
+def test_invalid(name, obj, primitive, errors):
+    if type(obj) == type:
+        should_fail = {"load": lambda: obj.load(primitive), "from_json": lambda: obj.from_json(json.dumps(primitive))}
+    else:
+        should_fail = {
+            "dump": obj.dump,
+            "validate": obj.validate,
+            "load": lambda: obj.__class__.load(primitive),
+            "to_json": obj.to_json,
+            "from_json": lambda: obj.__class__.from_json(json.dumps(primitive)),
+        }
+
+    for name, method in should_fail.items():
+        with pytest.raises(ValidationException) as excinfo:
+            method()
+
+        if excinfo.value.errors != errors:
+            pytest.fail("\nMethod: {}\nErrors:\n{}\nExpected:\n{}\n".format(name, excinfo.value.errors, errors))
+
+
+@pytest.mark.parametrize("name, obj, primitive", valid_test_cases)
+def test_valid_str_repr(name, obj, primitive):
+    assert str(obj) == str(obj.__dict__)
+
+
+@pytest.mark.parametrize("name, obj, primitive, errors", invalid_test_cases)
+def test_invalid_str_repr(name, obj, primitive, errors):
+    if type(obj) != type:
+        assert str(obj) == str(obj.__dict__)
+
+
+def test_validation_exception_str_repr():
+    e = ValidationException({"key": "value"})
+    assert str(e) == '{\n    "key": "value"\n}'
