de7a2acd03fca85b82d7b565ae5db5aad95fdfc8
Nils Barlaug <nils.barlaug@cognite.com>
2019-02-18 16:41:33 +0100

    Add ScheduleDataSpec

--- tests/tests_unit/test_utils.py
+++ tests/tests_unit/test_utils.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 import pytest
 
-from cognite.data_fetcher._utils import granularity_to_ms, time_interval_to_ms, timestamp_to_ms
+from cognite.data_fetcher._utils import granularity_to_ms, time_interval_to_ms, time_offset_to_ms, timestamp_to_ms
 
 
 class TestTimestampToMs:
@@ -25,26 +25,26 @@ class TestTimestampToMs:
     @pytest.mark.parametrize(
         "time_ago_string, expected_timestamp",
         [
-            ("now", 10 ** 9),
-            ("1s-ago", 10 ** 9 - 1 * 1000),
-            ("13s-ago", 10 ** 9 - 13 * 1000),
-            ("1m-ago", 10 ** 9 - 1 * 60 * 1000),
-            ("13m-ago", 10 ** 9 - 13 * 60 * 1000),
-            ("1h-ago", 10 ** 9 - 1 * 60 * 60 * 1000),
-            ("13h-ago", 10 ** 9 - 13 * 60 * 60 * 1000),
-            ("1d-ago", 10 ** 9 - 1 * 24 * 60 * 60 * 1000),
-            ("13d-ago", 10 ** 9 - 13 * 24 * 60 * 60 * 1000),
-            ("1w-ago", 10 ** 9 - 1 * 7 * 24 * 60 * 60 * 1000),
-            ("13w-ago", 10 ** 9 - 13 * 7 * 24 * 60 * 60 * 1000),
+            ("now", 10 ** 12),
+            ("1s-ago", 10 ** 12 - 1 * 1000),
+            ("13s-ago", 10 ** 12 - 13 * 1000),
+            ("1m-ago", 10 ** 12 - 1 * 60 * 1000),
+            ("13m-ago", 10 ** 12 - 13 * 60 * 1000),
+            ("1h-ago", 10 ** 12 - 1 * 60 * 60 * 1000),
+            ("13h-ago", 10 ** 12 - 13 * 60 * 60 * 1000),
+            ("1d-ago", 10 ** 12 - 1 * 24 * 60 * 60 * 1000),
+            ("13d-ago", 10 ** 12 - 13 * 24 * 60 * 60 * 1000),
+            ("1w-ago", 10 ** 12 - 1 * 7 * 24 * 60 * 60 * 1000),
+            ("13w-ago", 10 ** 12 - 13 * 7 * 24 * 60 * 60 * 1000),
         ],
     )
     def test_time_ago(self, time_mock, time_ago_string, expected_timestamp):
-        time_mock.return_value = 1000000
+        time_mock.return_value = 10 ** 9
 
         assert timestamp_to_ms(time_ago_string) == expected_timestamp
 
     @pytest.mark.parametrize("time_ago_string", ["1s", "4h", "13m-ag", "13m ago", "bla"])
-    def test_invalid_time_ago(self, time_ago_string):
+    def test_invalid(self, time_ago_string):
         with pytest.raises(ValueError, match=time_ago_string):
             timestamp_to_ms(time_ago_string)
 
@@ -57,6 +57,11 @@ class TestTimestampToMs:
 
         time_now = timestamp_to_ms("now")
         assert abs(expected_time_now - time_now) > 90
+
+    @pytest.mark.parametrize("t", [-1, datetime(1969, 12, 31), "100000000w-ago"])
+    def test_negative(self, t):
+        with pytest.raises(ValueError, match="negative"):
+            timestamp_to_ms(t)
 
 
 class TestTimeIntervalToMs:
@@ -96,6 +101,11 @@ class TestTimeIntervalToMs:
         with pytest.raises(ValueError, match=time_interval_string):
             time_interval_to_ms(time_interval_string)
 
+    @pytest.mark.parametrize("time_interval", [0, -123, timedelta(seconds=0), timedelta(seconds=-123)])
+    def test_non_positive_intervals(self, time_interval):
+        with pytest.raises(ValueError, match="positive"):
+            time_interval_to_ms(time_interval)
+
 
 class TestGranularityToMs:
     @pytest.mark.parametrize(
@@ -118,3 +128,41 @@ class TestGranularityToMs:
     def test_to_ms_invalid(self, granularity):
         with pytest.raises(ValueError, match=granularity):
             granularity_to_ms(granularity)
+
+
+class TestTimeOffsetToMs:
+    @pytest.mark.parametrize("t", [None, 1.23, [], {}])
+    def test_invalid_type(self, t):
+        with pytest.raises(TypeError, match="must be"):
+            time_offset_to_ms(t)
+
+    def test_ms(self):
+        assert 60000 == time_offset_to_ms(60000)
+        assert 123456789 == time_offset_to_ms(123456789)
+
+    def test_timedelta(self):
+        assert -2 * 60 * 1000 == time_offset_to_ms(timedelta(minutes=-2))
+        assert 3 * 60 * 60 * 1000 == time_offset_to_ms(timedelta(hours=3))
+
+    @pytest.mark.parametrize(
+        "time_offset_string, expected_ms",
+        [
+            ("1s", 1 * 1000),
+            ("-13s", -13 * 1000),
+            ("1m", 1 * 60 * 1000),
+            ("-13m", -13 * 60 * 1000),
+            ("1h", 1 * 60 * 60 * 1000),
+            ("-13h", -13 * 60 * 60 * 1000),
+            ("1d", 1 * 24 * 60 * 60 * 1000),
+            ("-13d", -13 * 24 * 60 * 60 * 1000),
+            ("1w", 1 * 7 * 24 * 60 * 60 * 1000),
+            ("-13w", -13 * 7 * 24 * 60 * 60 * 1000),
+        ],
+    )
+    def test_time_offset_string(self, time_offset_string, expected_ms):
+        assert time_offset_to_ms(time_offset_string) == expected_ms
+
+    @pytest.mark.parametrize("time_offset_string", ["13m-ago", "13", "bla"])
+    def test_time_offset_string_invalid(self, time_offset_string):
+        with pytest.raises(ValueError, match=time_offset_string):
+            time_offset_to_ms(time_offset_string)
