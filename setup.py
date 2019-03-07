1611af4eecb04cb10805ef47feec083b5d13a9e9
Erlend vollset <erlendvollset@gmail.com>
2019-03-07 19:53:51 +0100

    Use MANIFEST.in to discover python packages (#37)

--- setup.py
+++ setup.py
@@ -13,12 +13,6 @@ setup(
     url="",  # TODO
     author="Nils Barlaug",
     author_email="nils.barlaug@cognite.com",
-    packages=[
-        "cognite.model_hosting.data_fetcher",
-        "cognite.model_hosting.data_spec",
-        "cognite.model_hosting.schedules",
-        "cognite.model_hosting._cognite_model_hosting_common",
-    ],
     install_requires=["pandas", "marshmallow==3.0.0rc4", "requests>=2.21.0,<3.0.0"],
     python_requires=">=3.5",
 )
