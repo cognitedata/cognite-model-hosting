a99ba6b0bd6b5682a86f54beb25bdeac192d4cf4
Erlend vollset <erlendvollset@gmail.com>
2019-03-08 13:01:12 +0100

    Fix setup (#38)
    * Include packages in setup.py
    
    * Bump version
    
    * Remove print statemtn
    
    * Fix formatting
    
    * Use generated time series for tests

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import re
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 version = re.search(
     '^__version__\s*=\s*"(.*)"', open("cognite/model_hosting/_cognite_model_hosting_common/version.py").read(), re.M
@@ -13,6 +13,7 @@ setup(
     url="",  # TODO
     author="Nils Barlaug",
     author_email="nils.barlaug@cognite.com",
+    packages=["cognite.model_hosting." + p for p in find_packages(where="cognite/model_hosting")],
     install_requires=["pandas", "marshmallow==3.0.0rc4", "requests>=2.21.0,<3.0.0"],
     python_requires=">=3.5",
 )
