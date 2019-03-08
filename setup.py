4b5e09532ed39509c96d57e2beb64dffbd000cd9
erlendcognite <erlend.vollset@cognite.com>
2019-03-08 10:45:12 +0100

    Include packages in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import re
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
+print(["cognite.model_hosting." + p for p in find_packages(where="cognite/model_hosting")])
 version = re.search(
     '^__version__\s*=\s*"(.*)"', open("cognite/model_hosting/_cognite_model_hosting_common/version.py").read(), re.M
 ).group(1)
@@ -13,6 +13,7 @@ setup(
     url="",  # TODO
     author="Nils Barlaug",
     author_email="nils.barlaug@cognite.com",
+    packages=["cognite.model_hosting." + p for p in find_packages(where="cognite/model_hosting")],
     install_requires=["pandas", "marshmallow==3.0.0rc4", "requests>=2.21.0,<3.0.0"],
     python_requires=">=3.5",
 )
